5bcd8a42897365db1f4357a5c44b3faf6a430409
sundayifeanyi <thekingdomsunday@yahoo.com>
2021-04-17 03:08:43 +0100

    sample

--- 03_Day_Booleans_operators_date/03_day_starter/scripts/main.js
+++ 03_Day_Booleans_operators_date/03_day_starter/scripts/main.js
@@ -11,3 +11,32 @@ console.log(firstName)
 
 let empty = null
 console.log(empty) 
+
+let numOne = 4
+let numTwo = 3
+let sum = numOne + numTwo
+let diff = numOne - numTwo
+let mult = numOne * numTwo
+let div = numOne / numTwo
+let remainder = numOne % numTwo
+let powerOf = numOne ** numTwo
+
+console.log(sum, diff, mult, div, remainder, powerOf) // 7,1,12,1.33,1, 64
+
+const PI = 3.14
+let radius = 100          // length in meter
+
+//Let us calculate area of a circle
+const areaOfCircle = PI * radius * radius
+console.log(areaOfCircle)  //  314 m
+
+
+const gravity = 9.81      // in m/s2
+let mass = 72             // in Kilogram
+
+// Let us calculate weight of an object
+const weight = mass * gravity
+console.log(weight)        // 706.32 N(Newton)
+
+const boilingPoint = 100  // temperature in oC, boiling point of water
+const bodyTemp = 37       // b
